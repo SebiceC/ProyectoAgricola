954e7e04757371964d19a661a7015ebe472cca3b
SebiceC <sebartin9@gmail.com>
2025-02-22 16:53:53 -0500

    creacion del front, back y la conexion entre los dos

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola desde el backend!');
+app.use(cors());
+
+app.get('/api/message', (req, res) => {
+  res.json({ message:'¡Hola desde el backend!'});
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './api';
+
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    api.get('/message')
+    .then((response) => {
+      setMessage(response.data.message);
+    })
+    .catch((error) => {
+      console.error('Error al obtener el mensaje:', error);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Frontend con React y Axios</h1>
+      <p>{message}</p> 
     </div>
   );
 }
