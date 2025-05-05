8e266a9f62610a4afc0f114d4bba8e4a4b362ec1
SebiceC <sebartin9@gmail.com>
2025-05-05 18:55:23 -0500

    correcciones de errores en el modelo de cultivo

--- backend/src/models/cultivoModel.js
+++ backend/src/models/cultivoModel.js
@@ -1,20 +1,48 @@
-const db = require('../config/db');
+const pool = require('../config/db');
 
-const getCultivosConInfo = async () => {
-  const result = await db.query(`
-    SELECT 
-      c.id_cultivo,
-      c.nombre_cultivo,
-      u.nombre AS ubicacion,
-      s.tipo_suelo,
-      cs.eficiencia_riego
-    FROM cultivo c
-    JOIN cultivo_ubicacion cu ON c.id_cultivo = cu.id_cultivo
-    JOIN ubicacion u ON cu.id_ubicacion = u.ubicacion_id
-    JOIN cultivo_suelo cs ON c.id_cultivo = cs.id_cultivo
-    JOIN suelo s ON cs.id_suelo = s.id_suelo
-  `);
+const getAllCultivos = async () => {
+  const result = await pool.query('SELECT * FROM cultivo');
   return result.rows;
 };
 
-module.exports = { getCultivosConInfo };
+const getCultivoById = async (id) => {
+  const result = await pool.query('SELECT * FROM cultivo WHERE id_cultivo = $1', [id]);
+  return result.rows[0];
+};
+
+const createCultivo = async (cultivo) => {
+  const { nombre_cultivo, fecha_siembra, fecha_cosecha, profundidad_radicular, factor_agotamiento, factor_respuesta } = cultivo;
+  const result = await pool.query(
+    'INSERT INTO cultivo (nombre_cultivo, fecha_siembra, fecha_cosecha, profundidad_radicular, factor_agotamiento, factor_respuesta) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+    [nombre_cultivo, fecha_siembra, fecha_cosecha, profundidad_radicular, factor_agotamiento, factor_respuesta]
+  );
+  return result.rows[0];
+};
+
+const updateCultivo = async (id, cultivo) => {
+  const { nombre_cultivo, fecha_siembra, fecha_cosecha, profundidad_radicular, factor_agotamiento, factor_respuesta } = cultivo;
+  const result = await pool.query(
+    'UPDATE cultivo SET nombre_cultivo = $1, fecha_siembra = $2, fecha_cosecha = $3, profundiad_radicular = $4, factor_agotamiento = $5, factor_respuesta = $6, WHERE id_cultivo = $7 RETURNING *',
+    [nombre_cultivo,
+      fecha_siembra,
+      fecha_cosecha,
+      profundidad_radicular,
+      factor_agotamiento,
+      factor_respuesta,
+      id]
+  );
+  return result.rows[0];
+};
+
+const deleteCultivo = async (id) => {
+  const result = await pool.query('DELETE FROM cultivo WHERE id_cultivo = $1 RETURNING *', [id]);
+  return result.rows[0];
+};
+
+module.exports = {
+  getAllCultivos,
+  getCultivoById,
+  createCultivo,
+  updateCultivo,
+  deleteCultivo,
+};
